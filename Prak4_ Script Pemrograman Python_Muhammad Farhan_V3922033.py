52a80798cf85e5212e0772f0c450afdec53ab12e
Muhammad Farhan <115863849+Justify24@users.noreply.github.com>
2023-03-20 20:41:05 +0700

    Add files via upload

---
+++ Prak4_ Script Pemrograman Python_Muhammad Farhan_V3922033.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+Nama  : Muhammad Farhan
+Nim   : V3922033
+Kelas : TIE 22
+
+
+def hitung_fpb(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def hitung_kpk(a, b):
+    kpk = max(a, b)
+    while True:
+        if kpk % a == 0 and kpk % b == 0:
+            return kpk
+        kpk += 1
+
+def tampilkan_menu():
+    print("Pilihan menu:")
+    print("1. Hitung FPB")
+    print("2. Hitung KPK")
+    print("3. Keluar")
+
+def main():
+    while True:
+        tampilkan_menu()
+        pilihan = int(input("Masukkan pilihan Anda: "))
+
+        if pilihan == 1:
+            a = int(input("Masukkan bilangan pertama: "))
+            b = int(input("Masukkan bilangan kedua: "))
+            fpb = hitung_fpb(a, b)
+            print("FPB dari", a, "dan", b, "adalah", fpb)
+
+        elif pilihan == 2:
+            a = int(input("Masukkan bilangan pertama: "))
+            b = int(input("Masukkan bilangan kedua: "))
+            kpk = hitung_kpk(a, b)
+            print("KPK dari", a, "dan", b, "adalah", kpk)
+
+        elif pilihan == 3:
+            print("Terima kasih telah menggunakan program ini!")
+            break
+
+        else:
+            print("Pilihan tidak valid, silakan coba lagi.")
+
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
+
